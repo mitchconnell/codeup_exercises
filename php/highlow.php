115fb8b4034975a265abe2db1ca4d2973793a43f
mitchconnell <mitchconnell@hotmail.com>
2014-02-07 14:30:29 -0600

    Working high low game

--- php/highlow.php
+++ php/highlow.php
@@ -1,7 +1,7 @@
 <?php
  
 //create random numbers
-$random = rand(1 , 100);
+$random = mt_rand(1 , 100);
 
 //ask user to input numbers
 
@@ -9,23 +9,24 @@ fwrite(STDOUT, "hey loser I'm thinking of a number\n");
 
 //is it greater than
 
-do 
-{ 
+do { 
+	
 	$guess = fgets(STDIN);
+
 	if ($guess > $random) {
 
-	fwrite(STDOUT,"guess lower\n");
-			# code...
-		}	
-    if ($guess < $random) {
-    fwrite(STDOUT, "guess higher\n"); 	
+		fwrite(STDOUT,"guess lower\n");
+			
+	} else {
 
+   		fwrite(STDOUT, "guess higher\n");
     }
 
  	
- } while ($guess != $random); 
-	
+} while ($guess != $random);
 
+echo "winner\n"; 
+	
 
 //is it less than
 
