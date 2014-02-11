88091822cf5612d916b60b0ed54224f90d6e4cb8
Mitch <mitchconnell@hotmail.com>
2014-02-11 13:02:09 -0600

    arithmetic

---
+++ php/arithmetic.php
@@ -0,0 +1,40 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b . "\n";
+}
+
+function subtract($a, $b) {
+    // Add code here
+		echo $a - $b . "\n";
+}
+
+function multiply($a, $b) {
+    // Add code here
+		echo $a * $b . "\n";
+}
+
+function divide($a, $b) {
+    // Add code here
+		echo $a / $b . "\n";
+
+}
+
+function modulus($a, $b) {
+
+		echo $a % $b . "\n";
+
+}
+
+add(20,21);
+subtract(21,20);
+multiply(2,4);
+divide(3,7);
+modulus(5,9);		
+
+
+
+?>
+
+
+
