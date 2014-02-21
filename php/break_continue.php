0870dcf260537276c259a36648c5d257dfe83b61
Mitch <mitchconnell@hotmail.com>
2014-02-21 14:20:56 -0600

    break continue

---
+++ php/break_continue.php
@@ -0,0 +1,39 @@
+<?php
+
+for ($i = 1; $i <= 100; $i++) {
+	if ($i % 2 ==0){
+		continue;
+	} 
+	echo $i . "/n"; 
+
+
+for ($i = 1; $i <= 100; $i++) {
+	if ($i % 2 ==0){
+		break;
+		continue;
+	} 
+	echo $i . "/n";
+
+
+
+
+
+
+
+	
+}
+// $numbers = array(1);
+// // Loop 1
+// foreach ($numbers as $key => $value) {
+//     echo "{$value}\n";
+//     // Loop 2
+//     for ($i = 1; $i <= 100; $i++) {
+//         if ($i == 100) {
+//             echo "{$i}\n";
+//             break; 
+//             continue; 
+//         }
+//     }
+// }
+// echo "done!\n";
+
