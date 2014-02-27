fb8fdda67d75358fc5118a6933e51b0eca531b16
Mitch <mitchconnell@hotmail.com>
2014-02-27 17:49:18 -0600

    new arithmetic

--- php/arithmetic2.php
+++ php/arithmetic2.php
@@ -1,28 +1,54 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+	if (is_numeric($a) && is_numeric($b)){
+    	echo $a + $b;	
+	} else {
+		echo "ERROR: Both arguements must be numbers\n";
+	}	
 }
- add (2,7);
+ 	add (2,7);
 
 function subtract($a, $b) {
-    echo $a - $b;
-}
-subtract (2,9);
+	if (is_numeric($a) && is_numeric($b)){
+    	echo $a - $b;
+	} else {
+		echo "ERROR: Both arguements must be numbers\n";
+	}
+}	
+	subtract (2,9);
 
 function multiply($a, $b) {
-    echo $a * $b;
-}
-multiply (3,5);
+	if (is_numeric($a) && is_numeric($b)){
+    	echo $a * $b;
+	} else {
+		echo "ERROR: Both arguements must be numbers\n";
+	}
+}	
+
+	multiply (3,5);
 
 function divide($a, $b) {
-    echo $a / $b;
+	if (is_numeric($a) && is_numeric($b)){
+    	echo $a / $b;
+	} else {
+		echo "ERROR: Both arguements must be numbers\n";
+	}
+	if($b == 0) {
+		echo "ERROR:\n";
+		    
+	}
 }
 divide (2,2);
 
 function modulus($a, $b) {
+	if (is_numeric($a) && is_numeric($b)){
 	echo $a % $b;
+	} else {
+	echo "ERROR: Both arguements must be numbers\n";
+	}
 }
+
 modulus (9,5);
 
 
