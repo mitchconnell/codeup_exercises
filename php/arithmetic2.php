8722074638aa7087ec6ca009be7574813de9756f
Mitch <mitchconnell@hotmail.com>
2014-02-26 18:00:42 -0600

    New

---
+++ php/arithmetic2.php
@@ -0,0 +1,29 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b;
+}
+ add (2,7);
+
+function subtract($a, $b) {
+    echo $a - $b;
+}
+subtract (2,9);
+
+function multiply($a, $b) {
+    echo $a * $b;
+}
+multiply (3,5);
+
+function divide($a, $b) {
+    echo $a / $b;
+}
+divide (2,2);
+
+function modulus($a, $b) {
+	echo $a % $b;
+}
+modulus (9,5);
+
+
+?>
